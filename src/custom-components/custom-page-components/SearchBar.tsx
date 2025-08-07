ac733004acca057604510eb03dd4d36f8fecd8bf
AleksySzlapa <szlapaaleksy@gmail.com>
2025-08-07 07:46:34 +0200

    style: update SearchBar

--- src/custom-components/custom-page-components/SearchBar.tsx
+++ src/custom-components/custom-page-components/SearchBar.tsx
@@ -9,14 +9,14 @@ type Props = {
 
 const SearchBar = ({ searchTerm, onSearchTermChange,  }: Props) => {
   return (
-    <div className={cn("relative w-full max-w-80")}>
-      <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+    <div className={cn("relative w-full max-w-55")}>
+      <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-zinc-400" />
       <Input
         type="search"
-        placeholder="Search projects..."
+        placeholder="Search"
         value={searchTerm}
         onChange={(e) => onSearchTermChange(e.target.value)}
-        className="w-full pl-9 border-zinc-400 focus-visible:ring-1 focus-visible:ring-zinc-500 [&::-webkit-search-cancel-button]:hidden" // Full width within max-w-80 container
+        className="w-full pl-9 border-zinc-600 focus-visible:ring-1 focus-visible:ring-zinc-500 [&::-webkit-search-cancel-button]:hidden" // Full width within max-w-80 container
       />
     </div>
   );
